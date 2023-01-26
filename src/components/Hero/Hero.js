bf5520fad9f423a557b17d25642369efd946b753
philiphbk <damifaj101@gmail.com>
2023-01-26 12:21:05 +0100

    Added Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
+const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
@@ -14,7 +14,10 @@ const Hero = (props) => (
       <SectionText>
         I am a software engineer and cyber security expert with a passion for learning and building new things across different fields. I am an idealist and creative person who loves to bring ideas to life using technologies. I am also a computer science graduate student at the Federal University of Technology, Akure.
       </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
     </LeftSection>
+    
+
   </Section>
 );
 
