9895788770010082c4e7fd337b267e3de68ad97e
philiphbk <damifaj101@gmail.com>
2023-01-26 15:56:08 +0100

    Done Projects, Technologies, Animation

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,49 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+const projectsDemo = [{
+  title: 'Project 1',	
+  description: "This is a really long description about this project."
+}, {
+  title: 'Project 2',
+  description: "This is a really long description about this project."
+}, {
+  title: 'Project 3',
+  description: "This is a really long description about this project."
+},{
+  title: 'Project 4',
+  description: "This is a really long description about this project."
+}];
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(({id, image, title, description, tags, source, visit}) => (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree>{title}</HeaderThree>
+            <Hr />
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>Code</ExternalLinks>
+              <ExternalLinks href={source}>Source</ExternalLinks>
+            </UtilityList>
+          </TitleContent>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
